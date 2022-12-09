50b5b7c49d9685fc198701329b1ffd4232126909
lambda <hirn.j@husky.neu.edu>
2022-12-09 02:10:42 -0500

    improved solution

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -9,6 +9,31 @@ public sealed class Day08 : CustomDirBaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
+    private IEnumerable<Tree> GetTrees(string input)
+    {
+        var heightGrid = GetGrid(input);
+
+        var yMax = heightGrid.Count;
+        var xMax = heightGrid[0].Count;
+        // create trees
+        var treeGrid = heightGrid
+            .Select((row, y) => row.Select((height, x) => new Tree(height, $"({x},{y})")).ToList())
+            .ToList();
+
+        // link trees
+        for (var y = 0; y < heightGrid.Count; y++)
+        for (var x = 0; x < heightGrid[0].Count; x++)
+        {
+            var t = treeGrid[y][x];
+            if (x > 0) t.SetNeighbour(Direction.West, treeGrid[y][x - 1]);
+            if (x < xMax - 1) t.SetNeighbour(Direction.East, treeGrid[y][x + 1]);
+            if (y > 0) t.SetNeighbour(Direction.North, treeGrid[y - 1][x]);
+            if (y < yMax - 1) t.SetNeighbour(Direction.South, treeGrid[y + 1][x]);
+        }
+
+        return treeGrid.SelectMany(l => l);
+    }
+
     private List<List<int>> GetGrid(string input)
     {
         return Utils.GetLines(input)
@@ -17,85 +42,86 @@ public sealed class Day08 : CustomDirBaseDay
             .ToList();
     }
 
-    private int CountBetween(
-        IReadOnlyList<List<int>> grid,
-        int xStart, int xEnd, int xStep, Func<int, int, bool> xComp,
-        int yStart, int yEnd, int yStep, Func<int, int, bool> yComp,
-        ISet<string> seenPositions)
-    {
-        var currMax = int.MinValue;
-        var count = 0;
-        for (var y = yStart; yComp(y, yEnd); y += yStep)
-        for (var x = xStart; xComp(x, xEnd); x += xStep)
-        {
-            var positionString = $"{x}-{y}";
-            // Console.WriteLine(positionString);
-            if (grid[y][x] > currMax)
-            {
-                currMax = grid[y][x];
-                if (!seenPositions.Contains(positionString))
-                {
-                    count++;
-                    seenPositions.Add(positionString);
-                }
-            }
-        }
-
-        return count;
-    }
-
-    private int CountVisibleGrid(List<List<int>> grid)
-    {
-        var countTotal = 0;
-        var seenPositions = new HashSet<string>();
-
-        var yMax = grid.Count;
-        var xMax = grid[0].Count;
-
-        bool Lt(int a, int b)
-        {
-            return a < b;
-        }
-
-        bool Gte(int a, int b)
-        {
-            return a >= b;
-        }
-
-        bool Eq(int a, int b)
-        {
-            return a == b;
-        }
-
-        // count everything going horizontally
-        for (var y = 0; y < yMax; y++)
-        {
-            countTotal += CountBetween(grid, 0, xMax, 1, Lt, y, y, 1, Eq, seenPositions);
-            countTotal += CountBetween(grid, xMax - 1, 0, -1, Gte, y, y, 1, Eq, seenPositions);
-        }
-
-        // count everything going vertically
-        for (var x = 0; x < xMax; x++)
-        {
-            countTotal += CountBetween(grid, x, x, 1, Eq, 0, yMax, 1, Lt, seenPositions);
-            countTotal += CountBetween(grid, x, x, 1, Eq, yMax - 1, 0, -1, Gte, seenPositions);
-        }
-
-        return countTotal;
-    }
-
     public override ValueTask<string> Solve_1()
     {
-        var grid = GetGrid(_input);
+        var trees = GetTrees(_input);
+        var result = trees
+            .Where(t => t.CanSeeOutsideAnyDir());
 
-        var result = CountVisibleGrid(grid);
-        return new ValueTask<string>(result.ToString());
+        return new ValueTask<string>(result.Count().ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var result = "";
-        throw new NotImplementedException();
-        // return new ValueTask<string>(result.ToString());
+        var trees = GetTrees(_input);
+        var result = trees
+            .Select(t => t.GetTreeOverviewScore())
+            .Max();
+        return new ValueTask<string>(result.ToString());
+    }
+
+    private static IEnumerable<Direction> GetAllDirections()
+    {
+        return Enum
+            .GetValues(typeof(Direction))
+            .Cast<Direction>();
+    }
+
+    private enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    private class Tree
+    {
+        private readonly int _height;
+        private readonly string _location;
+        private readonly Dictionary<Direction, Tree> _neighbours;
+
+        public Tree(int height, string location)
+        {
+            _height = height;
+            _neighbours = new Dictionary<Direction, Tree>();
+            _location = location;
+        }
+
+        public override string ToString()
+        {
+            return $"Tree [height={_height}] [Pos={_location}]";
+        }
+
+        public void SetNeighbour(Direction d, Tree t)
+        {
+            _neighbours[d] = t;
+        }
+
+        private bool CanSeeOutsideInDirection(Direction d, int height)
+        {
+            return !_neighbours.ContainsKey(d) ||
+                   (height > _neighbours[d]._height && _neighbours[d].CanSeeOutsideInDirection(d, height));
+        }
+
+        public bool CanSeeOutsideAnyDir()
+        {
+            return GetAllDirections().Any(d => CanSeeOutsideInDirection(d, _height));
+        }
+
+        public int CountVisibleTrees(Direction d, int height)
+        {
+            if (!_neighbours.ContainsKey(d)) return 0;
+            var neighbour = _neighbours[d];
+            if (neighbour._height < height) return 1 + neighbour.CountVisibleTrees(d, height);
+            return 1;
+        }
+
+        public int GetTreeOverviewScore()
+        {
+            return GetAllDirections()
+                .Select(d => CountVisibleTrees(d, _height))
+                .Aggregate((a, b) => a * b);
+        }
     }
 }
