e7a7e0aa34ea8ed10010479588f1d2adea1ad3b1
lambda <hirn.j@husky.neu.edu>
2022-12-16 00:52:18 -0500

    day15 cleaned up part 1 to make part 2 easier

--- AdventOfCode/Day15.cs
+++ AdventOfCode/Day15.cs
@@ -12,12 +12,10 @@ public sealed class Day15 : CustomDirBaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    private ScanData ParseInput(string input)
+    private static IEnumerable<SensorRange> ParseInput(string input)
     {
-        var minX = int.MaxValue;
-        var maxX = int.MinValue;
         var lineRe = new Regex(@"Sensor at x=(-?\d+), y=(-?\d+):.*at x=(-?\d+), y=(-?\d+)");
-        var sensorRanges = Utils.GetLines(input)
+        return Utils.GetLines(input)
             .Select(line => Utils.GetAllRegexMatches(lineRe, line))
             .Select(elems => elems.Select(int.Parse))
             .Select(elems =>
@@ -25,14 +23,11 @@ public sealed class Day15 : CustomDirBaseDay
                 var l = elems.ToList();
                 var sensor = new Vector2Int(l[0], l[1]);
                 var beacon = new Vector2Int(l[2], l[3]);
-                minX = Math.Min(Math.Min(minX, sensor.X), beacon.X);
-                maxX = Math.Max(Math.Max(maxX, sensor.X), beacon.X);
                 return new SensorRange(sensor, sensor.ManhattanDistanceTo(beacon));
             }).ToList();
-        return new ScanData(sensorRanges, minX, maxX);
     }
 
-    private static List<Interval> MergeIntervals(List<Interval> lst)
+    private static IEnumerable<Interval> MergeIntervals(List<Interval> lst)
     {
         lst.Sort((a, b) => a.Start - b.Start);
 
@@ -58,56 +53,98 @@ public sealed class Day15 : CustomDirBaseDay
         return stack.ToList();
     }
 
-    private int CountPossibleBeaconPositions(ScanData scanData, int scanY)
+    // private static IEnumerable<Interval> GetMergedIntervalsHorizontal(IEnumerable<SensorRange> scanData, int scanY)
+    // {
+    //     var intervals = scanData
+    //         .Select(sensor =>
+    //         {
+    //             var distToScanLevel = sensor.Position.ManhattanDistanceTo(sensor.Position with { Y = scanY });
+    //             var rangeRemaining = sensor.Range - distToScanLevel;
+    //             if (rangeRemaining < 0) return new Interval(0, 0);
+    //             var xMin = sensor.Position.X - rangeRemaining;
+    //             var xMax = sensor.Position.X + rangeRemaining;
+    //             return new Interval(xMin, xMax);
+    //         })
+    //         .Where(x => !x.IsZero())
+    //         .ToList();
+    //     return MergeIntervals(intervals);
+    // }
+
+    private static IEnumerable<Interval> GetMergedIntervals(IEnumerable<SensorRange> scanData, int fixedScanVal,
+        bool horizontal)
     {
-        var intervals = scanData.SensorRanges
-            .Select(x =>
+        var intervals = scanData
+            .Select(sensor =>
             {
-                var distToScanLevel = x.Sensor.ManhattanDistanceTo(x.Sensor with { Y = scanY });
-                var rangeRemaining = x.Range - distToScanLevel;
-
+                var pointOnScanLevelLine = horizontal
+                    ? sensor.Position with { Y = fixedScanVal }
+                    : sensor.Position with { X = fixedScanVal };
+                var distToScanLevel = sensor.Position.ManhattanDistanceTo(pointOnScanLevelLine);
+                var rangeRemaining = sensor.Range - distToScanLevel;
                 if (rangeRemaining < 0) return new Interval(0, 0);
-
-                var xMin = x.Sensor.X - rangeRemaining;
-                var xMax = x.Sensor.X + rangeRemaining;
-                return new Interval(xMin, xMax);
+                var variableVal = horizontal ? sensor.Position.X : sensor.Position.Y;
+                var min = variableVal - rangeRemaining;
+                var max = variableVal + rangeRemaining;
+                return new Interval(min, max);
             })
             .Where(x => !x.IsZero())
             .ToList();
+        return MergeIntervals(intervals);
+    }
 
-        var mergedIntervals = MergeIntervals(intervals);
-        return mergedIntervals.Select(x => x.CountRange()).Sum();
+    private int CountPossibleBeaconPositions(IEnumerable<SensorRange> scanData, int fixedScanVal, bool horizontal)
+    {
+        var intervals = GetMergedIntervals(scanData, fixedScanVal, horizontal);
+        return intervals.Select(x => x.CountRange()).Sum();
+    }
+
+    private int CountPossibleBeaconPositionsWithLimit(IEnumerable<SensorRange> scanData, int scanY, Interval allowed,
+        bool horizontal)
+    {
+        var intervals = GetMergedIntervals(scanData, scanY, horizontal);
+        return intervals.Select(interval =>
+            new Interval(
+                Math.Max(allowed.Start, interval.Start),
+                Math.Min(allowed.End, interval.End)).CountRange()
+        ).Sum();
+    }
+
+    private Vector2Int FindPossiblePosition(IReadOnlyCollection<SensorRange> scanData, Interval possibleX,
+        Interval possibleY)
+    {
+        var expectedHorizontalCount = possibleX.CountRange();
+        var y = Enumerable.Range(possibleY.Start, possibleY.End)
+            .First(y =>
+                CountPossibleBeaconPositionsWithLimit(scanData, y, possibleX, true) < expectedHorizontalCount);
+
+        var expectedVerticalCount = possibleX.CountRange();
+        var x = Enumerable.Range(possibleX.Start, possibleX.End)
+            .First(x =>
+                CountPossibleBeaconPositionsWithLimit(scanData, x, possibleY, false) < expectedVerticalCount);
+
+        return new Vector2Int(x, y);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var scanData = ParseInput(_input);
-        var result = CountPossibleBeaconPositions(scanData, 2_000_000);
+        var scanData = ParseInput(_input).ToList();
+        var result = CountPossibleBeaconPositions(scanData, 2_000_000, true);
         return new ValueTask<string>(result.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
+        var scanData = ParseInput(_input).ToList();
         var result = "";
-        throw new NotImplementedException();
-        // return new ValueTask<string>(result.ToString());
+        //     FindPossiblePosition(
+        //     scanData, 
+        //     new Interval(0, 20),
+        //     new Interval(0, 20)
+        //     // new Interval(0, 4_000_000),
+        //     // new Interval(0, 4_000_000)
+        // );
+        return new ValueTask<string>(result);
     }
 
-    private record Interval(int Start, int End)
-    {
-        public bool IsZero()
-        {
-            return Start == 0 && End == 0;
-        }
-
-        public int CountRange()
-        {
-            return End - Start;
-        }
-    }
-
-
-    private record SensorRange(Vector2Int Sensor, int Range);
-
-    private record ScanData(IEnumerable<SensorRange> SensorRanges, int MinX, int MaxX);
+    private record SensorRange(Vector2Int Position, int Range);
 }
---
+++ AdventOfCode/Models/Interval.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Models;
+
+public record Interval(int Start, int End)
+{
+    public bool IsZero()
+    {
+        return Start == 0 && End == 0;
+    }
+
+    public int CountRange()
+    {
+        return End - Start;
+    }
+}
